60be2ea98a4e4c7b63aa6ad177ec342416f7d4d5
slowpon <slowpon@outlook.com>
2022-07-19 03:44:01 -0400

    reference

---
+++ multi-reference/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut s = String::from("ok");
+    let mut s1 = String::from("ok2");
+
+    let s2 = &mut s;
+    
+    // s2 = String::from("fd");
+    // rust中一个value不能同时被两个&mut reference，可以理解为value在任何时刻尤其仅有一个owner
+    // 但如果超出了ownership，就没什么问题了（因为之前的reference将被释放）
+    // let s2 = &mut s;
+    // 对&mut reference再进行一次reference是可以的
+    // let s2 = &mut s1;
+    println!("Hello, world!{}",s);
+}
